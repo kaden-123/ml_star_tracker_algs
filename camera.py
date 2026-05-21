8e51d14a2529bd2cfcdad6ae371ab7379fbbdbda
Kaden Wu <kadenlinwu@gmail.com>
2026-05-21 16:38:14 -0700

    rework camera, now with euler/quaternion

--- camera.py
+++ camera.py
@@ -1,110 +1,81 @@
 import numpy as np
-import matplotlib as mlp
 import pandas as pd
-import random
-import quaternion
-from PIL import Image
-from scipy.ndimage import gaussian_filter
-from scipy import ndimage
 
 class Camera:
-    def __init__(self, fov, resolution, data):
-        self.fov = np.deg2rad(fov);
-        self.resolution = resolution
+    def __init__(self, data):
+        self.data = data
+        self.direction  = np.zeros(4)
 
-        self.direction = np.zeros(3)
-
-        coords = data[["x", "y", "z"]].to_numpy()
+        coords = self.data[["x", "y", "z"]].to_numpy()
         coords /= np.linalg.norm(coords, axis = 1, keepdims=1) 
         self.data = pd.concat([pd.DataFrame(coords, columns = ["ux", "uy", "uz"], index=data.index), data], axis=1)
+         
+    def celest_point(self, ra, dec, roll):
+        """ point cam to given ra, dec, and roll."""
+        yaw = ra / 12 
+        pitch = -dec
+        self.direction = e_to_q(np.array([roll, pitch, yaw]))
         
-    def point_random(self):
-        random.seed(1)
-        self.direction = np.random.normal(size=3)
-        self.direction /= np.linalg.norm(self.direction)
-        self._filter()
-
-    def point_to_vec(self, vec):
-        self.direction = vec / np.linalg.norm(vec)
-        self._filter()
-
-    def point(self, catalog, starID):
-        mask = self.data[catalog] == starID
-        self.direction = np.array(self.data[mask][["ux", "uy", "uz"]]) 
-        self.direction = self.direction.reshape(3,)
-
-        self._filter()
+    def id_point(self, id, roll):
+        """ points camera to star given hr id """
+        mask = self.data["hr"] == id
+        ra = self.data.loc[mask, "ra"].iloc[0] / 12
+        dec = -self.data.loc[mask, "dec"].iloc[0]
+        self.direction = e_to_q(np.array([roll, dec, ra]))
         
-    def prep(self):
-        #find x and y axis unit vectors for projection
-        tmp = np.array([0, 0, 1])
-        xc = np.cross(self.direction, tmp)
-        xc /= np.linalg.norm(xc)
-        yc = np.cross(self.direction, xc)
-        yc /= np.linalg.norm(yc)
-
-        focal = (self.resolution[1] / 2) / np.tan(self.fov / 2)
-
-        #find star vec proj onto plane to find pixels
-        star_vec = self.data[["ux", "uy", "uz"]].to_numpy()
-        x_proj = np.dot(star_vec[:], xc)
-        y_proj = np.dot(star_vec[:], yc)
-        z_proj = np.dot(star_vec[:], self.direction)
+    def rand_point(self):
+        """ points to random direction (using shoemake alg) """
+        u1, u2, u3 = np.random.uniform(0, 1, 3)
+        w = np.sqrt(1 - u1) * np.sin(2 * np.pi * u2)
+        x = np.sqrt(1 - u1) * np.cos(2 * np.pi * u2)
+        y = np.sqrt(u1)     * np.sin(2 * np.pi * u3)
+        z = np.sqrt(u1)     * np.cos(2 * np.pi * u3)
+        self.direction = np.array([w, x, y, z])
         
-        px = (self.resolution[1] / 2) + focal * x_proj / z_proj
-        py = (self.resolution[0] / 2) - focal * y_proj / z_proj
+    def create_centroids(self, focal, res, pixel_pitch, sensor_size):
+        """ returns np array [id, px, py] of simulated visible centroids """
+        W, H = res
+        if sensor_size is not None:
+            sx, sy = sensor_size
+            pixel_pitch = sensor_size[0] / res[0]
+        elif pixel_pitch is not None:
+            sx = W * pixel_pitch   # sensor width  in mm
+            sy = H * pixel_pitch   # sensor height in mm
+            
+        fovx = 2 * np.arctan(sx / (2 * focal))   
+        fovy = 2 * np.arctan(sy / (2 * focal))
 
-        #append to dataframe
-        self.data = pd.concat([self.data, pd.DataFrame(px, columns=["px"]), pd.DataFrame(py, columns=["py"])], axis=1)
+        #use quaternion as a rotation to turn rotate into camera frame
+        #yada yah
+        celest_to_cam_DCM = (q_to_DCM(self.direction))
+        celest_star_direc = np.array(self.data[["ux", "uy", "uz"]])
+        cam_star_direc = celest_star_direc @ celest_to_cam_DCM.T
+        
+        #cam boresight is (1, 0, 0) i think z+ is more standard but should still work the same
+        angles_y = np.arctan2(cam_star_direc[:, 1], cam_star_direc[:, 0])
+        angles_z = np.arctan2(cam_star_direc[:, 2], cam_star_direc[:, 0])
 
-    def rotate_img(self, degrees):        
-        theta = np.deg2rad(degrees);
-        temp = np.quaternion(0, self.direction[0], self.direction[1], self.direction[2])
-        temp *= np.sin(theta / 2)
-        
-        q = temp + np.quaternion(np.cos(theta / 2),0,0, 0)
-        q_conj = q.conjugate()
-        
-        new_vecs = []
-        for index, row in self.data.iterrows():
-            sv = row[["ux", "uy", "uz"]].to_numpy()
-            v = np.quaternion(0, sv[0], sv[1], sv[2])
-            L = q * v * q_conj
-            new_vecs.append(np.array([L.x, L.y, L.z]))
-        
-        new_vecs = np.vstack(new_vecs)
-        self.data.loc[:, ["ux", "uy", "uz"]] = new_vecs
+        mask = (cam_star_direc[:, 0] > 0) & (np.abs(angles_y) <= fovx / 2) & (np.abs(angles_z) <= fovy / 2)
 
+        #reappend to keep ids and then filter out visible stars
+        ids = np.array(self.data["hr"])
+        centroids = (np.column_stack((ids.reshape(-1,1), cam_star_direc)))
+        centroids = centroids[mask]
 
-    def _filter(self):
-        #find dot between cam direction and star unit vector to filter seeable stars
-        dots = np.dot(self.data[["ux", "uy", "uz"]].to_numpy(), self.direction)
-        mask = (dots >= np.cos(self.fov / 2))
-        self.data = self.data[mask].reset_index(drop=True)
+        #project into image
+        f_px = focal / pixel_pitch
+        img_px = f_px * (centroids[:, 2] / centroids[:, 1]) + res[0] / 2
+        img_py = f_px * (centroids[:, 3] / centroids[:, 1]) + res[1] / 2 
         
-    def draw_img(self):
-        #image stuff yup and yes its kinda broken rn
-        self.image = np.zeros((self.resolution[0], self.resolution[1]), dtype=np.float32)
-        mag = self.data['mag'].to_numpy()
-        intensity = 10 ** (-0.4 * mag)
+        #return new pandas df (maybe just think of returning np directly)
+        return pd.DataFrame(np.column_stack([ids[mask], img_px, img_py]), columns=["hr", "px", "py"])
         
-        px = self.data['px'].to_numpy().astype(int)
-        py = self.data['py'].to_numpy().astype(int)
-                
-        self.image[py, px] = intensity
+    def rotate(self):
+        """ rotate camera using specified quaternion """
+        test = 1
         
-        self.image = gaussian_filter(self.image, sigma=2)
+    def roll(self, n):
+        """ roll camera orientation by n deg) """
+        test = 1
         
-        # self.image += np.random.normal(loc= 0.0001, scale = 0.0001, size = self.image.shape)
-        self.image = np.clip(self.image / self.image.max() * 255, 0, 255).astype(np.uint8)
-        im = Image.fromarray(self.image)
-        im.show()
-
-    #for the fact that i use inplace changes and i might mess things up
-    def reset_data(self, data):
-        coords = data[["x", "y", "z"]].to_numpy()
-        coords /= np.linalg.norm(coords, axis = 1, keepdims=1) 
-        self.data = pd.concat([pd.DataFrame(coords, columns = ["ux", "uy", "uz"], index=data.index), data], axis=1)
-
-    # def rotate_img(self, degrees):
-    #     self.image = ndimage.rotate(self.image, degrees, reshape=True)
+        
